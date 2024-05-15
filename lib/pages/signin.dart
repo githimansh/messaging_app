2e312d64109477787425463267ada8c8c937eaa2
Himanshu kumar <himu2486@gmail.com>
2024-05-15 23:28:58 +0530

    add the new code

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -13,8 +13,19 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       body: Container(child: Stack(children: [
       Container(
-     height: MediaQuery.of(context).size.height/3.5,
+     height: MediaQuery.of(context).size.height/4,
+     width: MediaQuery.of(context).size.width,
+
+     decoration: BoxDecoration(
+      gradient: LinearGradient(colors: [Color.fromARGB(255, 122, 223, 543), Color.fromARGB(133, 233, 543, 122),],
+      begin: Alignment.topLeft,
+       end: Alignment.bottomRight,),
+       borderRadius: BorderRadius.vertical(bottom: Radius.elliptical(MediaQuery.of(context).size.width, 110.5), ),
+     ),
       ),
+      Column(children: [
+        Text("SignIn")
+      ],)
       ],),),
     );
   }
