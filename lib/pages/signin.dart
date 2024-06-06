03e5a051c594af4921a5e1660705f70f7317eab9
Himanshu kumar <himu2486@gmail.com>
2024-06-06 13:30:05 +0530

    add the new code

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
 
-  @override 
+  @override
   State<SignIn> createState() => _SignInState();
 }
 
@@ -11,22 +11,128 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Stack(children: [
-      Container(
-     height: MediaQuery.of(context).size.height/4,
-     width: MediaQuery.of(context).size.width,
-
-     decoration: BoxDecoration(
-      gradient: LinearGradient(colors: [Color.fromARGB(255, 122, 223, 543), Color.fromARGB(133, 233, 543, 122),],
-      begin: Alignment.topLeft,
-       end: Alignment.bottomRight,),
-       borderRadius: BorderRadius.vertical(bottom: Radius.elliptical(MediaQuery.of(context).size.width, 110.5), ),
-     ),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 4,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 122, 223, 543),
+                    Color.fromARGB(133, 233, 543, 122),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.elliptical(
+                      MediaQuery.of(context).size.width, 110.5),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 70),
+              child: Column(
+                children: [
+                  Center(
+                      child: Text(
+                    "SignIn",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold),
+                  )),
+                  Center(
+                      child: Text(
+                    "LogIn to your account",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w400),
+                  )),
+                  Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+                    child: Material(
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                        height: MediaQuery.of(context).size.height / 2,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Email",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 1, color: Colors.black38),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    prefixIcon: Icon(
+                                      Icons.mail_outline,
+                                      color: Color(0xFF7f30fe),
+                                    )),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Email",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 1, color: Colors.black38),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    prefixIcon: Icon(
+                                      Icons.mail_outline,
+                                      color: Color(0xFF7f30fe),
+                                    )),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-      Column(children: [
-        Text("SignIn")
-      ],)
-      ],),),
     );
   }
 }
